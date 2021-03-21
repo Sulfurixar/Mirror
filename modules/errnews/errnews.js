d8b037be5841e433d951df49565562f00ca97cd1
eltrigs-GL752VW-Linux <marekparts@gmail.com>
2021-03-21 17:21:36 +0200

    news is now displayed in a random order

--- modules/errnews/errnews.js
+++ modules/errnews/errnews.js
@@ -21,22 +21,24 @@ Module.register("errnews", {
 
 	socketNotificationReceived: function (notification, payload) {
 		if (notification === "DATA") {
-			this.dataFile = payload;
+			this.dataFile = payload[0];
+			this.randomArray = payload[1];
 			this.updateDom();
 		}
 	},
 
 	getDom: function () {
 		var wrapper = document.createElement("div");
+
 		if (this.dataFile) {
 			const title = document.createElement("div");
 			title.className = "newsfeed-title bright medium-large light";
-			title.innerHTML = this.dataFile[(this.newscounter * 5) % (Math.round(this.dataFile.length / 5) * 5)];
+			title.innerHTML = this.dataFile[(this.randomArray[this.newscounter] * 5) % (Math.round(this.dataFile.length / 5) * 5)];
 			wrapper.appendChild(title);
 
 			const description = document.createElement("div");
 			description.className = "newsfeed-desc medium light";
-			description.innerHTML = this.dataFile[(this.newscounter * 5 + 2) % (Math.round(this.dataFile.length / 5) * 5)];
+			description.innerHTML = this.dataFile[(this.randomArray[this.newscounter] * 5 + 2) % (Math.round(this.dataFile.length / 5) * 5)];
 			wrapper.appendChild(description);
 		} else {
 			wrapper.innerHTML = "No data";
--- modules/errnews/node_helper.js
+++ modules/errnews/node_helper.js
@@ -25,12 +25,34 @@ module.exports = NodeHelper.create({
 
 		//to read a file to do the following
 		fs.readFile("/home/eltrigs/Mirror/Mirror/modules/errnews/newsdata.txt", "utf8", (err, data) => {
-			let output_data = null;
+			let full_output_data = null;
 
 			if (!err) {
-				output_data = data.toString().concat("\n").split("\n");
+				let output_data = data.toString().concat("\n").split("\n");
+				let random_array = this.sendRandomArray(output_data);
+				full_output_data = [output_data, random_array];
 			}
-			this.sendSocketNotification("DATA", output_data);
+			this.sendSocketNotification("DATA", full_output_data);
 		});
+	},
+
+	sendRandomArray: function (output_data) {
+		for (var a = [], i = 0; i < output_data.length / 5; ++i) a[i] = i;
+
+		function shuffle(array) {
+			var tmp,
+				current,
+				top = array.length;
+			if (top)
+				while (--top) {
+					current = Math.floor(Math.random() * (top + 1));
+					tmp = array[current];
+					array[current] = array[top];
+					array[top] = tmp;
+				}
+			return array;
+		}
+
+		return shuffle(a);
 	}
 });
